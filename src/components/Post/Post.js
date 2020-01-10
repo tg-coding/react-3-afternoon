a6f141c084a5459c48da05e5c781dcd3158bf899
Tia Gollaher <tiaag2013@gmail.com>
2020-01-10 14:43:55 -0700

    finished

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -54,6 +54,7 @@ export default class Post extends Component {
       // const editing = this.state.editing
       // const showMasterMenu = this.state.showMasterMenu
     const { editing, showMasterMenu } = this.state;
+    const { text, date, deletePostFn, id} = this.props;
 
     return (
       // Main body of post
@@ -66,8 +67,9 @@ export default class Post extends Component {
           {/* Drop-down menu. Remember that the "showMasterMenu" variable has been destructured off of this.state */}
           <div className="Post__master-menu" style={ { display: showMasterMenu ? 'flex' : 'none' } }>
             <span onClick={ this.showEdit }>Edit</span>
-            <span>Delete</span>
+            <span onClick={() => deletePostFn(id)}>Delete</span>
           </div>
+        
         </div>
 
         {/* This is where all the meta data of the post will go (who, when, where) */}
@@ -79,7 +81,7 @@ export default class Post extends Component {
           <span className="Post__name">DevMountain</span>
           <span className="Post__handle">@DevMountain</span>
 
-          <span className="Post__date">- POST DATE GOES HERE</span>
+          <span className="Post__date">- {date}</span>
         </div>
 
         {/* This is where the text goes. Notice the turnary statement. The turnary statement decides to display either the text OR the editor view
@@ -92,13 +94,14 @@ export default class Post extends Component {
         */}
         <div className="Post__content">
           {
-            // This has been pulled off of this.state via destructuring
             editing
             ?
-              <Edit text=""
-                    hideEdit={ this.hideEdit } />
+              <Edit text={ text }
+                    id={ this.props.id }
+                    hideEdit={ this.hideEdit }
+                    updatePostFn={ this.props.updatePostFn } />
             :
-              <span className="Post__text">POST TEXT GOES HERE</span>
+              <span className="Post__text">{ text }</span>
           }
         </div>
 
